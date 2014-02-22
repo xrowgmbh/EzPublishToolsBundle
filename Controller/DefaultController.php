b21b716c0ae28b53f0e9a7e92195a006b64a5513
Cloud Default User <ec2-user@localhost.(none)>
2014-02-22 03:33:45 -0800

    save work

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -6,14 +6,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+
 class DefaultController extends Controller
 {
-    /**
+    /** test that worked with symfony 2.4
      * @Route("/hello/{name}")
      * @Template()
      */
     public function indexAction($name)
     {
         return array('name' => $name);
+    }
+    /**
+     * @Route("/_profiling/{name}")
+     * @Template()
+     */
+    public function profilingAction($name)
+    {
+        
+        $dir = $this->container->get('kernel')->getRootDir() . "/logs/profiling";
+        return $this->container->get('templating')->renderResponse('xrowBundleEzPublishToolsBundle:Default:profiling.html.twig', array(
+            'data'  => unserialize( file_get_contents( $dir . "/" . basename($name)))
+        ));
     }
 }
--- EventListener/LogStashListener.php
+++ EventListener/LogStashListener.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpKernel\Event\PostResponseEvent;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Filesystem\Filesystem;
 
 class LogStashListener implements EventSubscriberInterface
 {
@@ -18,7 +19,8 @@ class LogStashListener implements EventSubscriberInterface
     const DISABLED = 1;
 
     const ENABLED = 2;
-
+    const TYPE = 'ezpublish';
+    private $dir = null;
     private $profiler = null;
 
     private $exception = null;
@@ -35,13 +37,9 @@ class LogStashListener implements EventSubscriberInterface
     {
         $this->container = $container;
         if (function_exists('xhprof_enable') and $this->profiler === null) {
-            if (strpos($_SERVER["REQUEST_URI"], '/_') !== 0) {
-                xhprof_enable(XHPROF_FLAGS_NO_BUILTINS + XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
-                //xhprof_sample_enable();
-                $this->profiler = self::ENABLED;
-            } else {
-                $this->profiler = self::DISABLED;
-            }
+            xhprof_enable(XHPROF_FLAGS_NO_BUILTINS + XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
+            //xhprof_sample_enable();
+            $this->profiler = self::ENABLED;
         }
         $this->data["tags"] = array(
             'ezpublish'
@@ -50,14 +48,12 @@ class LogStashListener implements EventSubscriberInterface
         //@TODO Do the timezone right. #, new \DateTimeZone( date_default_timezone_get() ) , new \DateTimeZone( date_default_timezone_get() )
         $date = isset($_SERVER['REQUEST_TIME']) ? new \DateTime('@' . (int) $_SERVER['REQUEST_TIME'] ) : new DateTime('now', new \DateTimeZone( date_default_timezone_get() ));
         $this->data["@timestamp"] = $date->format(\DateTime::W3C);
+
+        $this->dir = $container->get('kernel')->getRootDir() . "/logs/profiling";  
     }
 
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
-        //@TODO this seems to be not called yet we need to replace ExceptionHandler, newer symfony version seem to listen to the function.
-        if ($this->onlyMasterRequests && ! $event->isMasterRequest()) {
-            return;
-        }
         $this->data["tags"][] = 'exception';
         $this->data["tags"][] = 'error';
         $this->data['trace'] = $event->getException()->getTrace();
@@ -80,7 +76,7 @@ class LogStashListener implements EventSubscriberInterface
         if (HttpKernelInterface::MASTER_REQUEST !== $event->getRequestType()) {
             return;
         }
-        $data['status'] = 1;
+        $this->data['status'] = 1;
 
     }
 
@@ -97,17 +93,20 @@ class LogStashListener implements EventSubscriberInterface
             }
         return $nowSSL;
     }
-
-    static function getURL()
+    static function getHost()
     {
         if ($_SERVER["SERVER_PORT"] != "80") {
-            $pageURL .= self::serverProtocol() . "://" . $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"] . $_SERVER["REQUEST_URI"];
+            $pageURL = $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"];
         } else {
-            $pageURL .= self::serverProtocol() . "://" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+            $pageURL = $_SERVER["SERVER_NAME"];
         }
         return $pageURL;
     }
-    static function serverProtocol()
+    static function getURL()
+    {
+        return self::getProtocol() . "://" . self::getHost() . $_SERVER["REQUEST_URI"];
+    }
+    static function getProtocol()
     {
         if ( self::isSSL() )
             return 'https';
@@ -119,58 +118,58 @@ class LogStashListener implements EventSubscriberInterface
         if (! self::isEnabled()) {
             return;
         }
-        $response = 
-        $data = $this->data;
-        
-        $data["type"] = "ezpublish";
-        $data["version"] = "1";
+        $this->data["type"] = "ezpublish";
+        $this->data["version"] = "1";
 
-        $data['environment'] = $this->container->get('kernel')->getEnvironment();
-        
-        $argvtmp = $argv;
-        $script = array_shift($argvtmp);
-        
+        $this->data['environment'] = $this->container->get('kernel')->getEnvironment();
+
         if (php_sapi_name() == 'cli') {
-            $data["source"] = getcwd() . $script;
-            $data["tags"][] = array(
+            $argvtmp = $GLOBALS['argv'];
+            $script = array_shift($argvtmp);
+            $this->data["source"] = getcwd() . $script;
+            $this->data["tags"][] = array(
                 'cli'
             );
-            $data['arguments'] = $argvtmp;
+            $this->data['arguments'] = $argvtmp;
         } else {
-            $data["source"] = self::getURL();
-            $data["message"] = self::getURL();
-            $data["tags"][] = array(
+            $this->data["source"] = self::getURL();
+            $this->data["message"] = self::getURL();
+            $this->data["tags"][] = array(
                 'http'
             );
             
-            $data['cookie'] = $_COOKIE;
-            $data['get'] = $_GET;
-            $data['post'] = $_POST;
+            $this->data['cookie'] = $_COOKIE;
+            $this->data['get'] = $_GET;
+            $this->data['post'] = $_POST;
             $protectkeys = array( 'password', 'Password' );
             foreach ( $protectkeys as $key )
             {
-                if (isset( $data['get'][$key] ))
+                if (isset( $this->data['get'][$key] ))
                 {
-                     unset($data['get']);
+                     unset($this->data['get']);
                 }
-                if (isset( $data['post'][$key] ))
+                if (isset( $this->data['post'][$key] ))
                 {
-                    unset($data['post']);
+                    unset($this->data['post']);
                 }
             }
             if (isset($_SERVER['SERVER_ADDR'])) {
                 $data['server_addr'] = $_SERVER['SERVER_ADDR'];
             }
-            $data['remote_addr'] = $_SERVER['REMOTE_ADDR'];
+            $this->data['remote_addr'] = $_SERVER['REMOTE_ADDR'];
             if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-                $data['remote_addr'] = $_SERVER['HTTP_X_FORWARDED_FOR'];
+                $this->data['remote_addr'] = $_SERVER['HTTP_X_FORWARDED_FOR'];
             }
-            $data['request_method'] = $_SERVER['REQUEST_METHOD'];
-            $data['http_referrer'] = $_SERVER['HTTP_REFERER'];
-            $data['agent'] = $_SERVER['HTTP_USER_AGENT'];
+            $this->data['request_method'] = $_SERVER['REQUEST_METHOD'];
+            if( isset($_SERVER['HTTP_REFERER']) )
+            {
+                $this->data['http_referrer'] = $_SERVER['HTTP_REFERER'];
+            }
+            
+            $this->data['agent'] = $_SERVER['HTTP_USER_AGENT'];
             
         }
-
+        
         /** @var $session \Symfony\Component\HttpFoundation\Session\Session */
         $session = $this->container->get( 'session' );
         /** @var $authenticationToken \Symfony\Component\Security\Core\Authentication\Token\TokenInterface */
@@ -178,23 +177,23 @@ class LogStashListener implements EventSubscriberInterface
         
         if ( $session->isStarted() )
         {
-            $data['ezpublish']['session'] = $session->getId();
+            $this->data['ezpublish']['session'] = $session->getId();
         }
         
         if( $session->isStarted() && $authenticationToken !== null )
         {
             $user = $authenticationToken->getUser();
-            $data['ezpublish']['groups'] = $user->getRoles();
-            $data['ezpublish']['username'] = $user->getUsername();
+            $this->data['ezpublish']['groups'] = $user->getRoles();
+            $this->data['ezpublish']['username'] = $user->getUsername();
         }
 
         if (isset($_SERVER['PHP_AUTH_USER'])) {
-            $data['http_auth_user'] = $_SERVER['PHP_AUTH_USER'];
+            $this->data['http_auth_user'] = $_SERVER['PHP_AUTH_USER'];
         }
         
         if (function_exists('posix_uname')) {
             $nodeinfo = posix_uname();
-            $data["source_host"] = $nodeinfo['nodename'];
+            $this->data["source_host"] = $nodeinfo['nodename'];
         }
         
         foreach (headers_list() as $header) {
@@ -204,37 +203,39 @@ class LogStashListener implements EventSubscriberInterface
             {
                  continue;
             }
-            $data['http_response_header'][$key] = $value;
+            $this->data['http_response_header'][$key] = $value;
         }
 
         if (function_exists('http_response_code')) {
-            $data['http_response_code'] = http_response_code();
+            $this->data['http_response_code'] = http_response_code();
         }
         else
         {
-            $data['http_response_code'] = $event->getResponse()->getStatusCode();
+            $this->data['http_response_code'] = $event->getResponse()->getStatusCode();
         }
-        $data['body_bytes_sent'] = strlen($event->getResponse()->getContent());
+        $this->data['body_bytes_sent'] = strlen($event->getResponse()->getContent());
         
-        foreach (getallheaders() as $key => $value) {
-            $data['http_request_header'][$key] = $value;
+        foreach (getallheaders() as $key => $value)
+        {
+            $this->data['http_request_header'][$key] = $value;
         }
-        /*
-        var_dump("no");
+
         $data = xhprof_disable();
-
-        $xhprof_runs = new XHProfRuns_Default();
-
-        $run_id = $xhprof_runs->save_run($data, "ezpublish");
-        
-        $data["fields"]['profiling'] = "http://<xhprof-ui-address>/index.php?run=$run_id&source=ezpublish";
+        $fs = new Filesystem();
+        if ( !$fs->exists( $this->dir ) )
+        {
+            $fs->mkdir( $this->dir, 0777 );
+        }
+        $xhprof = new \XHProfRuns_Default( $this->dir );
+        $run_id = $xhprof->save_run($data , self::TYPE);
+        $this->data['profiling'] = self::getProtocol() . "://" .  self::getHost() . "/_profiling/".$run_id.".".self::TYPE.".xhprof";
         $file = fopen("../ezpublish/logs/logstash.log", "a");
-        */
+        
         if ($file) {
-            fwrite($file, json_encode($data) . "\n");
+            fwrite($file, json_encode($this->data) . "\n");
             fclose($file);
         }
-        
+
     }
 
     public static function getSubscribedEvents()
